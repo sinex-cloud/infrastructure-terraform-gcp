0403e8578410da667b1ce3a95391b66e6cf5c6e2
sinex <ahmed.brini.abr@gmail.com>
2026-07-22 13:51:15 +0100

    Point module source at pinned git tag v0.1.0 instead of local path

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
 module "data_platform" {
-  # Local path during development. Once gcp-foundation-module is pushed to GitHub
-  # and tagged, swap this for the pinned remote ref:
-  # source = "git::https://github.com/<your-username>/gcp-foundation-module.git?ref=v0.1.0"
-  source = "../gcp-foundation-module"
+  source = "git::https://github.com/sinex-cloud/gcp-foundation-module.git?ref=v0.1.0"
+  # source = "../gcp-foundation-module"
 
   path_to_yaml_resources_file = "${path.module}/foundation-config/foundation.yaml"
   env                          = var.env
   project                      = var.project
 }
+

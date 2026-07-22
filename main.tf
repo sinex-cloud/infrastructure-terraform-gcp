ef97023d033649a053c8bf4bc302fbef21bbde76
sinex <ahmed.brini.abr@gmail.com>
2026-07-22 23:32:20 +0100

    Move to environment-scoped IAM (module v0.1.3); relocate dataset/bucket to europe-west1

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 module "data_platform" {
-  source = "git::https://github.com/sinex-cloud/gcp-foundation-module.git?ref=v0.1.0"
-  # source = "../gcp-foundation-module"
+  source = "git::https://github.com/sinex-cloud/gcp-foundation-module.git?ref=v0.1.3"
 
   path_to_yaml_resources_file = "${path.module}/foundation-config/foundation.yaml"
   env                          = var.env
